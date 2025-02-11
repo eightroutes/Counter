0c4c8476c700417b1618bec0bcb8b265a1826084
EllGHT <52287330+eightroutes@users.noreply.github.com>
2025-02-11 17:47:45 +0900

    fix

--- PreCampCounter/ViewController.swift
+++ PreCampCounter/ViewController.swift
@@ -1,19 +1,41 @@
-//
-//  ViewController.swift
-//  PreCampCounter
-//
-//  Created by 정근호 on 2/11/25.
-//
-
 import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var numberText: UILabel!
+    @IBOutlet weak var resetBtn: UIButton!
+    @IBOutlet weak var plusBtn: UIButton!
+    @IBOutlet weak var minusBtn: UIButton!
+    
+    var currentNum = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        updateUI()
     }
 
+    @IBAction func plusBtnClicked(_ sender: Any) {
+        currentNum += 1
+        updateUI()
+    }
 
+    @IBAction func minusBtnClicked(_ sender: Any) {
+        currentNum -= 1
+        updateUI()
+    }
+
+    @IBAction func resetBtnClicked(_ sender: Any) {
+        currentNum = 0
+        updateUI()
+    }
+    
+    private func updateUI() {
+        numberText.text = "\(currentNum)"
+        updateButtonStates()
+    }
+
+    private func updateButtonStates() {
+        plusBtn.isEnabled = currentNum < 10
+        minusBtn.isEnabled = currentNum > -10
+    }
 }
-
